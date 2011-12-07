e83ab9df2f9df399df4edcfb45a24f4d79431bcc
Mikel Larreategi <mlarreategi@codesyntax.com>
2011-12-07 09:07:15 +0000

    changes to simplyfy view of the minutes

--- cs/aktak/browser/aktak_urteka.py
+++ cs/aktak/browser/aktak_urteka.py
@@ -1,28 +1,19 @@
-
-__version__ = '$Id$'
-
-from Products.CMFPlone import utils
-
-
-
-__version__ = '$Id$'
-
-from Products.CMFPlone import utils
-
-import DateTime
-import pdb
 from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
-from Products.CMFCore.utils import getToolByName
-import random
-class Aktak(BrowserView):
 
-	def __call__(self):
-                
-		context=aq_inner(self.context)
-                urteak=context.getFolderContents({'portal_type':'DonEdukia'})
-                dict={}
-                for urtea in urteak:
-                    aktak=urtea.getObject().getFolderContents({'portal_type':'File'}, full_objects=1)
-                    dict[urtea.Title]=aktak
-                return dict
+class AktakUrteka(BrowserView):
+    def __call__(self):
+        context = self.context
+        return Aktak(context, self.request).items()
+
+class Aktak(BrowserView):
+    
+    def items(self):        
+        context = aq_inner(self.context)
+        urteak = context.getFolderContents({'portal_type':'DonEdukia'})
+        dict = {}
+        for urtea in urteak:
+            aktak = urtea.getObject().getFolderContents({'portal_type':'File'}, full_objects=1)
+            dict[urtea.Title] = aktak
+
+        return dict
